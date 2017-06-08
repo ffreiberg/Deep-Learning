09584d81dff76dde303d6ab9351f8bc628149abb
Fabian <fafreibe@htwg-kosntanz.de>
2017-06-08 15:04:48 +0200

    not sure if output correct

--- aufgabe 5/aufgabe 5.py
+++ aufgabe 5/aufgabe 5.py
@@ -12,19 +12,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def ctc(prob, seq):
+def ctc(probs, seq, name):
 
     # Thus, instead of considering a label ℓ, we consider a modified label L,
     # which is just ℓ with blanks inserted between all letters, as well as at the beginning and end.
-    L = 2 * len(seq) + 1    # length of sequence [9]
-    T = prob.shape[1]       # timesteps [1, ..., 12]
+    L = 2 * len(seq) + 1     # length of sequence [9]
+    T = probs.shape[1]       # timesteps [1, ..., 12]
     blank = 0
 
     a = np.zeros((L, T))
     b = np.zeros((L, T))
 
-    a[0, 0] = prob[blank, 0]
-    a[1, 0] = prob[seq[0], 0]
+    seqProb = ord(seq[0]) - 96
+
+    a[0, 0] = probs[blank, 0]
+    a[1, 0] = probs[seqProb, 0]
 
     c = np.sum(a[:, 0])
     a[:, 0] /= c
@@ -39,27 +41,79 @@ def ctc(prob, seq):
 
             if(s % 2 == 0):
                 if(s == 0):
-                    a[s, t] = a[s, t - 1] * prob[blank, t]
+                    a[s, t] = a[s, t - 1] * probs[blank, t]
                 else:
-                    a[s, t] = (a[s, t - 1] + a[s - 1, t - 1]) * prob[blank, t]
-            elif(s == 1 or seq[l] == seq[l - 1]):
-                a[s, t] = (a[s, t - 1] + a[s - 1, t - 1]) * prob[seq[l], t]
+                    a[s, t] = (a[s, t - 1] + a[s - 1, t - 1]) * probs[blank, t]
+            elif(s == 1 or seq[int(l)] == seq[int(l - 1)]):
+                a[s, t] = (a[s, t - 1] + a[s - 1, t - 1]) * probs[(ord(seq[int(l)]) - 96), t]
             else:
-                a[s, t] = (a[s, t - 1] + a[s - 1, t - 1] + a[s-2, t - 1]) * prob[seq[l], t]
+                a[s, t] = (a[s, t - 1] + a[s - 1, t - 1] + a[s - 2, t - 1]) * probs[ord(seq[int(l)]) - 96, t]
 
         c = np.sum(a[start:end, t])
         a[start:end, t] /= c
         forward += np.log(c)
 
-        print(forward)
+    seqProb = ord(seq[-1]) - 96
 
-    pass
+    b[-1, -1] = probs[0, -1]
+    b[-2, -1] = probs[seqProb, -1]
 
+    c = np.sum(b[:, -1])
+    b[:, -1] /= c
+
+    backward = np.log(c)
+
+    for t in range(T - 2, -1, -1):
+        start = max(0, L - 2 * (T - t))
+        end = min(2 * t + 2, L)
+        for s in range(end - 1, -1, -1):
+            l = (s - 1) / 2
+
+            if(s % 2 == 0):
+                if(s == L - 1):
+                    b[s, t] = b[s, t + 1] * probs[0, t]
+                else:
+                    b[s, t] = (b[s, t + 1] + b[s + 1, t + 1] * probs[0, t])
+            elif(s == L - 2 or seq[int(l)] == seq[int(l + 1)]):
+                b[s, t] = (b[s, t + 1] + b[s + 1, t + 1]) * probs[ord(seq[int(l)]) - 96, t]
+            else:
+                b[s, t] = (b[s, t + 1] + b[s + 1, t + 1] + b[s + 2, t + 1]) * probs[ord(seq[int(l)]) - 96, t]
+
+        c = np.sum(b[start:end, t])
+        b[start:end, t] /= c
+        backward += np.log(c)
+
+    grad = np.zeros(probs.shape)
+    p = a * b
+    for s in range(L):
+        if(s % 2 == 0):
+            grad[0, :] += p[s, :]
+            #p[s, :] /= probs[0, :]
+        else:
+            grad[ord(seq[int((s - 1) / 2)]) - 96, :] += p[s, :]
+            #p[s, :] /= probs[ord(seq[int((s - 1) / 2)]) - 96, :]
+
+    #grad = probs - grad / (probs * np.sum(p, axis=0))
+
+    print(forward, backward, '\n', grad)
+    axes = plt.gca()
+    axes.set_xlim([0, 11])
+    #axes.set_ylim([0,  1])
+    plt.ylabel('probability')
+    plt.xlabel('timesteps')
+    plt.title('CTC output of {} distribution'.format(name))
+    plt.plot(grad[0].T, 'k--', label='$\\varepsilon$')
+    plt.plot(grad[1].T, label='a')
+    plt.plot(grad[2].T, label='b')
+    plt.plot(grad[3].T, label='c')
+    plt.legend()
+    plt.savefig('ctc_{}_distribution'.format(name))
+    plt.show()
 
 def main():
 
     '''
-    equal distribution:
+    uniform distribution:
 
     ykt     t=1  t=2  t=3  t=4  t=5  t=6  t=7  t=8  t=9  t=10 t=11 t=12
     k=ε     0.25 0.25 0.25 0.25 0.25 0.25 0.25 0.25 0.25 0.25 0.25 0.25
@@ -78,13 +132,15 @@ def main():
     '''
 
     seq = 'abbc'
-
-    probEqual = np.array([
+    '''
+    probUniform = np.array([
         [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25],
         [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25],
         [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25],
         [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
     ])
+    '''
+    probUniform = np.full((4, 12), 0.25)
 
     probWeighted = np.array([
         [.25, .25, .0, .0, .0, .5, .5, .25, .0, .0, .25, .25],
@@ -93,7 +149,14 @@ def main():
         [ .0,  .0, .0, .0, .0, .0, .0, .25, .5, .5,  .5, .75],
     ])
 
-    ctc(probEqual, seq)
+    probs = [probUniform, probWeighted]
+    names = ['uniform',     'weighted']
+
+    tpl = zip(probs, names)
+
+    for i in tpl:
+        p, n = i[0], i[1]
+        ctc(p, seq, n)
 
     pass
 
